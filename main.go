07d5f41e8a1b1d74dcf45a7c6070f7fea4c754a7
Chris <crmackay@gmail.com>
2015-02-19 15:47:48 -0500

    testing part 1...fastq io

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 //
 
-
 package main
 
 import (
-        "fmt"
-        bio "github.com/crmackay/gobioinfo"
-        )     
+	"fmt"
+	bio "github.com/crmackay/gobioinfo"
+)
 
 /*
 func work (input chan FASTARead, ouput chan FASTARead) {
@@ -14,8 +13,7 @@ func work (input chan FASTARead, ouput chan FASTARead) {
     fmt.Println("Starting Worker:")
 }*/
 
-
-// this function uses the `select` method to keep IO to just one goroutine 
+// this function uses the `select` method to keep IO to just one goroutine
 // this combined with our CPU accounting allows for fast loading and offloading
 // of data to disk, while ensuring that that maximum CPU utilization is happening
 
@@ -41,46 +39,47 @@ func work (input chan FASTARead, ouput chan FASTARead) {
                 }
         }
     }
-    
-    
-    
-    
+
+
+
+
 }
 */
 
 func main() {
-    
-    //totalCPUS := 4
-    
-    //CPUWorkers := totalCPUS - 1
-    
-    //TEMP set number of CPUs to 
-    
-    //rawReads := chan FASTARead
-    
-    //processedReads := chan FASTARead
-    
-    // TODO parse command line arguments
-    
-        // TODO set number of CPUs
-        
-        // TODO set path to input file
-        
-        // TODO set path to output file
-    
-    // TODO start file parser
-    
-    // TODO start file writer
-    //for c := 0; c < numberCPUs; c++ {
-        //go work(rawReads, processedReads)
-    //}
-    
-    // TODO start CPU# of workers
-    
-    // wait until the are all done
-    
-    bio.Testfastq()
-    bio.Testalign()
-    
-    fmt.Println("this is the main function")
+
+	//totalCPUS := 4
+
+	//CPUWorkers := totalCPUS - 1
+
+	//TEMP set number of CPUs to
+
+	//rawReads := chan FASTARead
+
+	//processedReads := chan FASTARead
+
+	// TODO parse command line arguments
+
+	// TODO set number of CPUs
+
+	// TODO set path to input file
+
+	// TODO set path to output file
+
+	// TODO start file parser
+
+	// TODO start file writer
+	//for c := 0; c < numberCPUs; c++ {
+	//go work(rawReads, processedReads)
+	//}
+
+	// TODO start CPU# of workers
+
+	// wait until the are all done
+
+	//bio.Testfastq()
+	//bio.Testalign()
+	bio.Testfastqwriter()
+
+	fmt.Println("this is the main function")
 }
